56197b4c3dd0465c12da9d39dad7335df1723a4d
aishivam <shivamhande0000@gmail.com>
2021-04-21 11:58:25 +0530

    Projects and profiles add

---
+++ src/Components/Projects.js
@@ -0,0 +1,197 @@
+import { Component } from "react"
+import * as React from 'react';
+import p1 from '../images/projects/robot.png'
+import p2 from '../images/projects/engineering.png'
+import p3 from '../images/projects/chip.png'
+import man from '../images/projects/man.png'
+export default class Projects extends Component{
+
+  
+render(){
+    return(
+        <div>
+
+        <div className='Projectdash container '>
+        <p className="h3">Top Projects</p>
+            <div className="slidecard card-body">
+                <div className="d-flex justify-content-center">
+                <img  className="circle img-fluid" src={p1} />
+                </div>
+                <h5 className="card-title text-center">Name</h5>
+                <p className="card-text">Hello hi sjdkjdvnmm,asjyhbsdui </p>
+                <a href="#" className="btn btn-primary">Buy</a>
+            </div>
+
+            <div className="slidecard card-body">
+                <div className="d-flex justify-content-center">
+                <img  className="circle img-fluid" src={p2} />
+                </div>
+                <h5 className="card-title text-center">Name</h5>
+                <p className="card-text">Hello hi sjdkjdvnmm,asjyhbsdui </p>
+                <a href="#" className="btn btn-primary">Buy</a>
+            </div>
+
+            <div className="slidecard card-body">
+                <div className="d-flex justify-content-center">
+                <img  className="circle img-fluid" src={p3} />
+                </div>
+                <h5 className="card-title text-center">Name</h5>
+                <p className="card-text">Hello hi sjdkjdvnmm,asjyhbsdui </p>
+                <a href="#" className="btn btn-primary">Buy</a>
+            </div>
+   
+        </div>
+
+       
+        <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+        <p className="h3">Top Performer</p>
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <div class="row gx-5">
+                        <div class="col">
+                            <div className="profile card-body">
+                            <div className="d-flex justify-content-center">
+                            <img  className="pic img-fluid" src={man} />
+                            </div>
+                            <h5 className="card-title text-center">Name</h5>
+                            <p className="card-text">Hello hi sjdkjdvnmm,asjyhbsdui </p>
+                        </div>
+                        </div>
+                        <div class="col">
+                            <div className="profile card-body">
+                            <div className="d-flex justify-content-center">
+                            <img  className="pic img-fluid" src={man} />
+                            </div>
+                            <h5 className="card-title text-center">Name</h5>
+                            <p className="card-text">Hello hi sjdkjdvnmm,asjyhbsdui </p>
+                        </div>
+                        </div>
+                        <div class="col">
+                        <div className="profile card-body">
+                            <div className="d-flex justify-content-center">
+                            <img  className="pic img-fluid" src={man} />
+                            </div>
+                            <h5 className="card-title text-center">Name</h5>
+                            <p className="card-text">Hello hi sjdkjdvnmm,asjyhbsdui </p>
+                        </div>
+                        </div>
+                    </div>
+                
+                    
+                </div>
+                <div class="carousel-item">
+                <div class="row gx-5">
+                        <div class="col">
+                            <div className="profile card-body">
+                            <div className="d-flex justify-content-center">
+                            <img  className="pic img-fluid" src={man} />
+                            </div>
+                            <h5 className="card-title text-center">Name</h5>
+                            <p className="card-text">Hello hi sjdkjdvnmm,asjyhbsdui </p>
+                        </div>
+                        </div>
+                        <div class="col">
+                            <div className="profile card-body">
+                            <div className="d-flex justify-content-center">
+                            <img  className="pic img-fluid" src={man} />
+                            </div>
+                            <h5 className="card-title text-center">Name</h5>
+                            <p className="card-text">Hello hi sjdkjdvnmm,asjyhbsdui </p>
+                        </div>
+                        </div>
+                        <div class="col">
+                        <div className="profile card-body">
+                            <div className="d-flex justify-content-center">
+                            <img  className="pic img-fluid" src={man} />
+                            </div>
+                            <h5 className="card-title text-center">Name</h5>
+                            <p className="card-text">Hello hi sjdkjdvnmm,asjyhbsdui </p>
+                        </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                <div class="row gx-5">
+                        <div class="col">
+                            <div className="profile card-body">
+                            <div className="d-flex justify-content-center">
+                            <img  className="pic img-fluid" src={man} />
+                            </div>
+                            <h5 className="card-title text-center">Name</h5>
+                            <p className="card-text">Hello hi sjdkjdvnmm,asjyhbsdui </p>
+                        </div>
+                        </div>
+                        <div class="col">
+                            <div className="profile card-body">
+                            <div className="d-flex justify-content-center">
+                            <img  className="pic img-fluid" src={man} />
+                            </div>
+                            <h5 className="card-title text-center">Name</h5>
+                            <p className="card-text">Hello hi sjdkjdvnmm,asjyhbsdui </p>
+                        </div>
+                        </div>
+                        <div class="col">
+                        <div className="profile card-body">
+                            <div className="d-flex justify-content-center">
+                            <img  className="pic img-fluid" src={man} />
+                            </div>
+                            <h5 className="card-title text-center">Name</h5>
+                            <p className="card-text">Hello hi sjdkjdvnmm,asjyhbsdui </p>
+                        </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                <div class="row gx-5">
+                        <div class="col">
+                            <div className="profile card-body">
+                            <div className="d-flex justify-content-center">
+                            <img  className="pic img-fluid" src={man} />
+                            </div>
+                            <h5 className="card-title text-center">Name</h5>
+                            <p className="card-text">Hello hi sjdkjdvnmm,asjyhbsdui </p>
+                        </div>
+                        </div>
+                        <div class="col">
+                            <div className="profile card-body">
+                            <div className="d-flex justify-content-center">
+                            <img  className="pic img-fluid" src={man} />
+                            </div>
+                            <h5 className="card-title text-center">Name</h5>
+                            <p className="card-text">Hello hi sjdkjdvnmm,asjyhbsdui </p>
+                        </div>
+                        </div>
+                        <div class="col">
+                        <div className="profile card-body">
+                            <div className="d-flex justify-content-center">
+                            <img  className="pic img-fluid" src={man} />
+                            </div>
+                            <h5 className="card-title text-center">Name</h5>
+                            <p className="card-text">Hello hi sjdkjdvnmm,asjyhbsdui </p>
+                        </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+            </a>
+            <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+            </a>
+        </div>
+
+
+
+
+    </div>
+
+
+
+
+    )
+}
+
+}
