ae9504b127cf96c462836407f7fd9f00ec637844
aishivam <shivamhande0000@gmail.com>
2021-04-28 18:20:53 +0530

    Updates

---
+++ precache-manifest.f27092ca74faff2de2f9a178a6a9d55c.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "acb512453e493f18606c5f27d1093281",
+    "url": "/project_rexora_01/index.html"
+  },
+  {
+    "revision": "7294822382cdbb426bf1",
+    "url": "/project_rexora_01/static/css/2.9233ae65.chunk.css"
+  },
+  {
+    "revision": "2bde5925a70f9dc27330",
+    "url": "/project_rexora_01/static/css/main.5b6cf621.chunk.css"
+  },
+  {
+    "revision": "7294822382cdbb426bf1",
+    "url": "/project_rexora_01/static/js/2.c65d8bec.chunk.js"
+  },
+  {
+    "revision": "2bde5925a70f9dc27330",
+    "url": "/project_rexora_01/static/js/main.b490b811.chunk.js"
+  },
+  {
+    "revision": "ada196c19712361fe49b",
+    "url": "/project_rexora_01/static/js/runtime~main.b29afb96.js"
+  },
+  {
+    "revision": "0de9abcb92011d145b984a0cdbf36b2f",
+    "url": "/project_rexora_01/static/media/1.0de9abcb.png"
+  },
+  {
+    "revision": "af174559dd29ceb82559cba31b81d8d7",
+    "url": "/project_rexora_01/static/media/2.af174559.png"
+  },
+  {
+    "revision": "e7be3ecd921efc88bbca5e090862eac7",
+    "url": "/project_rexora_01/static/media/3.e7be3ecd.png"
+  },
+  {
+    "revision": "b3a8637f09fb1f7fe9206cc1c70d337c",
+    "url": "/project_rexora_01/static/media/4.b3a8637f.png"
+  },
+  {
+    "revision": "326ccf369d71e401f8206a475215a0e9",
+    "url": "/project_rexora_01/static/media/engineering.326ccf36.png"
+  },
+  {
+    "revision": "47305402a5e102588c9946a65939438a",
+    "url": "/project_rexora_01/static/media/man.47305402.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3073a1206f87d85c40920f56789338e8.js"
+  "/project_rexora_01/precache-manifest.f27092ca74faff2de2f9a178a6a9d55c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/project_rexora_01/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
