db6bbdf3e971a0a6a4e1ef52bc1d5adfae714959
aishivam <shivamhande0000@gmail.com>
2021-05-06 08:43:12 +0530

    Updates

---
+++ precache-manifest.30d5bddf07d3c65f1c77b9e26d4ed93e.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "400d435610c5ff02a7f4e9b233f1e24e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5324cc0c91c6ff1c7030",
+    "url": "/static/css/2.9233ae65.chunk.css"
+  },
+  {
+    "revision": "f005a29b34309123538f",
+    "url": "/static/css/main.5b94e103.chunk.css"
+  },
+  {
+    "revision": "5324cc0c91c6ff1c7030",
+    "url": "/static/js/2.6dbff5f8.chunk.js"
+  },
+  {
+    "revision": "f005a29b34309123538f",
+    "url": "/static/js/main.aa0e101b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "0de9abcb92011d145b984a0cdbf36b2f",
+    "url": "/static/media/1.0de9abcb.png"
+  },
+  {
+    "revision": "af174559dd29ceb82559cba31b81d8d7",
+    "url": "/static/media/2.af174559.png"
+  },
+  {
+    "revision": "e7be3ecd921efc88bbca5e090862eac7",
+    "url": "/static/media/3.e7be3ecd.png"
+  },
+  {
+    "revision": "b3a8637f09fb1f7fe9206cc1c70d337c",
+    "url": "/static/media/4.b3a8637f.png"
+  },
+  {
+    "revision": "326ccf369d71e401f8206a475215a0e9",
+    "url": "/static/media/engineering.326ccf36.png"
+  },
+  {
+    "revision": "47305402a5e102588c9946a65939438a",
+    "url": "/static/media/man.47305402.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/project_rexora_01/precache-manifest.f27092ca74faff2de2f9a178a6a9d55c.js"
+  "/precache-manifest.30d5bddf07d3c65f1c77b9e26d4ed93e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/project_rexora_01/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
